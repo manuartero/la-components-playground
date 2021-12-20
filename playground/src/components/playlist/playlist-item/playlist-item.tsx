dd1aabcfc3b1799dc3a8c45016a756895e384544
Manu Artero Anguita <manutero.developer@gmail.com>
2021-12-20 13:10:21 +0100

    pairing-session 20-dic
    Co-authored-by: idelafuente <idelafuente@tuenti.com>

---
+++ playground/src/components/playlist/playlist-item/playlist-item.tsx
@@ -0,0 +1,129 @@
+import * as React from 'react';
+import './playlist-item.scss';
+import classnames from 'classnames';
+import { humanize } from '../../../utils/seconds';
+
+enum FocusedElement {
+    PLAY_BUTTON,
+    INFO_BUTTON,
+}
+
+export interface PlayListItemProps {
+    id: string;
+    thumbnail?: string;
+    type: 'with-thumbnail' | 'no-thumbnail';
+    title: string;
+    description: string;
+    durationInSeconds: number;
+    progressInSeconds: number;
+    isPlaying: boolean;
+    isFocused: boolean;
+    onPlay: () => void;
+    onPause: () => void;
+}
+
+const PlaylistItem = ({
+    id,
+    thumbnail,
+    title,
+    description,
+    durationInSeconds,
+    progressInSeconds,
+    isPlaying,
+    isFocused,
+    onPlay,
+    onPause,
+}: PlayListItemProps) => {
+    const [isShowingInfo, setIsShowingInfo] = React.useState(false);
+    const [focusedElement, setFocusedElement] = React.useState<FocusedElement>(FocusedElement.PLAY_BUTTON);
+
+    React.useEffect(() => {
+        if (!isFocused) {
+            setIsShowingInfo(false);
+        }
+    }, [isFocused]);
+
+    const alreadyStarted = progressInSeconds > 0;
+
+    const isPlayButtonFocused = isFocused && focusedElement == FocusedElement.PLAY_BUTTON;
+
+    const isInfoButtonFocused = isFocused && focusedElement == FocusedElement.INFO_BUTTON;
+
+    const seconds = () => {
+        const { hh, mm, ss } = humanize(durationInSeconds);
+        if (hh > 0) {
+            return `${hh} hr ${mm} min ${ss}`;
+        }
+        return `${mm}:${ss}`;
+    };
+
+    const handleInfo = () => {
+        setIsShowingInfo(!isShowingInfo);
+    };
+
+    return (
+        <div key={id} className='SDK__playlist-item' data-test-id={`playlist-item-${id}`}>
+            <div className='SDK__playlist-item__header'>
+                <div className='SDK__playlist-item__header__thumbnail'>
+                    <div
+                        className='SDK__playlist-item__header__thumbnail__img'
+                        style={{ backgroundImage: `url(${thumbnail})` }}
+                    ></div>
+                    <div
+                        className={classnames('SDK__playlist-item__header__thumbnail__wave', {
+                            ['SDK__playlist-item__header__thumbnail__wave--hidden']: !isPlaying,
+                        })}
+                    ></div>
+                </div>
+                <div className='SDK__playlist-item__header__info'>
+                    <h3>{title}</h3>
+                    <p>{seconds()}</p>
+                    <p>{progressInSeconds}</p>
+                </div>
+                <div className='SDK__playlist-item__header__actions'>
+                    {isPlaying ? (
+                        <div
+                            className={classnames('SDK__playlist-item__header__actions__pause', {
+                                ['SDK__playlist-item__header__actions__pause--focused']: isPlayButtonFocused,
+                            })}
+                            role='button'
+                            onClick={onPause}
+                        ></div>
+                    ) : (
+                        <div
+                            className={classnames('SDK__playlist-item__header__actions__play', {
+                                ['SDK__playlist-item__header__actions__play--focused']: isPlayButtonFocused,
+                            })}
+                            role='button'
+                            onClick={onPlay}
+                        ></div>
+                    )}
+                    {isShowingInfo ? (
+                        <div
+                            className={classnames('SDK__playlist-item__header__actions__close', {
+                                ['SDK__playlist-item__header__actions__close--focused']: isInfoButtonFocused,
+                            })}
+                            role='button'
+                            onClick={handleInfo}
+                        ></div>
+                    ) : (
+                        <div
+                            className={classnames('SDK__playlist-item__header__actions__info', {
+                                ['SDK__playlist-item__header__actions__info--focused']: isInfoButtonFocused,
+                            })}
+                            role='button'
+                            onClick={handleInfo}
+                        ></div>
+                    )}
+                </div>
+            </div>
+            {isShowingInfo && (
+                <div className='SDK__playlist-item__description'>
+                    <div>{description}</div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default PlaylistItem;
--- playground/src/index.js
+++ playground/src/index.js
@@ -3,7 +3,6 @@ import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style/index.scss';
 
 SDK.setListener('APP', onAppMessage);
 
